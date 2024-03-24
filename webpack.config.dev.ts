04aa899e507de7b483f0616dca61a08b470f5ab6
Miroslav Vdoviak <miroslav.vdoviak@emplifi.io>
2024-03-24 13:35:27 +0100

    [issue #11168] [web] příprava webu pro production prostředí

--- webpack.config.dev.ts
+++ webpack.config.dev.ts
@@ -1,38 +1,20 @@
-import * as path from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import * as path from 'path'
 import webpack, { Configuration } from 'webpack'
+import { merge } from 'webpack-merge'
+import commonConfig from './webpack.config'
 
-const config: Configuration = {
+const config: Configuration = merge(commonConfig, {
 	entry: [
 		path.resolve(__dirname, './src/client/index.tsx'),
 		'webpack-hot-middleware/client?quiet=true',
 	],
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
 	mode: 'development',
 	devtool: 'source-map',
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'public'),
-	},
 	plugins: [
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			template: path.resolve(__dirname, './src/client/public/index.html'),
-		}),
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.NoEmitOnErrorsPlugin(),
 	],
-}
+})
 
 export default config
---
+++ webpack.config.ts
@@ -0,0 +1,34 @@
+import * as path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import { Configuration } from 'webpack'
+
+const config: Configuration = {
+	entry: path.resolve(__dirname, './dist/src/client/index.js'),
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	mode: 'production',
+	target: 'web',
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		publicPath: '/',
+		filename: '[name].js',
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: path.resolve(__dirname, './src/client/public/index.html'),
+		}),
+	],
+}
+
+export default config
