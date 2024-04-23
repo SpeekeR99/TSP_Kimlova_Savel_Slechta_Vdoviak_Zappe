ad7ad93c88a762fe38eddcabfddfd245fcee4b2e
Jakub Slechta <jakub.slechta@emplifi.io>
2024-04-23 16:21:06 +0200

    issue #11315 Nasazení webové aplikace s využitím docker registry

---
+++ scripts/build-publish-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# $1 - name of the image (PACKAGE_NAME)
+IMG_NAME="$CI_REGISTRY_IMAGE/$1:latest"
+
+echo "$CI_REGISTRY_PASSWORD" | docker login $CI_REGISTRY -u $CI_REGISTRY_USER --password-stdin
+cd web
+docker build -t $IMG_NAME .
+docker push $IMG_NAME
