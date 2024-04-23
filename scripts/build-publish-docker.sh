e664e8906c0e6644a14c3b6c5f9de4cdb23dc48a
Jakub Slechta <jakub.slechta@emplifi.io>
2024-04-23 16:46:20 +0200

    fix

--- scripts/build-publish-docker.sh
+++ scripts/build-publish-docker.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+# $1 is the name of the image (PACKAGE_NAME)
 IMG_NAME="$CI_REGISTRY_IMAGE/$1:latest"
 
-echo "$CI_REGISTRY_PASSWORD" | docker login $CI_REGISTRY -u $CI_REGISTRY_USER --password-stdin
 cd web
+ls -la
+docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 docker build -t $IMG_NAME .
 docker push $IMG_NAME
