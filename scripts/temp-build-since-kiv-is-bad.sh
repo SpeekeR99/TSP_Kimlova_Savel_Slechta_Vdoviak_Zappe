868b5af374526ab8b06ba9aea4a246673cf038f6
Jakub Slechta <jakub.slechta@emplifi.io>
2024-05-12 19:11:23 +0200

    issue #11316 #11315 #11424 nasazení mongodb, web a ai částí na nuadu

--- scripts/temp-build-since-kiv-is-bad.sh
+++ scripts/temp-build-since-kiv-is-bad.sh
@@ -2,9 +2,29 @@
 
 REGISTRY_USER="jamesari"
 REGISTRY="registry.gitlab.com"
-IMG_NAME="$REGISTRY/$REGISTRY_USER/aswi-testing/web-dist:latest"
+WEB_IMG_NAME="$REGISTRY/$REGISTRY_USER/aswi-testing/web-dist:latest"
+AI_IMG_NAME="$REGISTRY/$REGISTRY_USER/aswi-testing/ai-dist:latest"
 
 echo "$ASWI_GITLAB_TOKEN" | docker login $REGISTRY -u $REGISTRY_USER --password-stdin
-cd web
-docker build --platform linux/amd64 -t $IMG_NAME .
-docker push $IMG_NAME
+
+build_and_push_image() {
+	local directory=$1
+	local image_name=$2
+	
+	cd "$directory"
+	
+	echo "Building image: $image_name"
+	docker build --platform linux/amd64 -t "$image_name" .
+	
+	echo "Pushing image: $image_name"
+	docker push "$image_name"
+}
+
+if [ "$1" = "web" ]; then
+	build_and_push_image "web" "$WEB_IMG_NAME"
+elif [ "$1" = "ai" ]; then
+	build_and_push_image "ai" "$AI_IMG_NAME"
+else
+	build_and_push_image "web" "$WEB_IMG_NAME"
+	build_and_push_image "../ai" "$AI_IMG_NAME"
+fi
